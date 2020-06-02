ccddce92abe83d66f4698b19f3225c5a7d68ae22
github username <github email address>
2020-06-02 09:33:48 -0700

    oo intro

---
+++ 02-oo-intro/README.md
@@ -0,0 +1,44 @@
+## Ruby OO
+
+Plants => 1. teach Ruby what a plant is
+
+# Questions
+    - instance variables 
+    - instance methods
+    - why use an attr writer vs accessor
+
+# Objects
+    - What is an object?
+        - symbols that represent the binary code 
+            - dictionary with key/value pairs
+                - kind of like a hash
+            - Contain instructions on performing a task
+                - can invoke methods
+
+    - Why do we use objects?
+        - A thing
+        - modularity
+        - contributes to the bigger picture of everything
+
+    - What objects have we worked with?
+        - "string"
+        - classes
+        - arrays
+        - integer
+        - float
+        - hash
+
+    Everything is an object => Object Oriented Program
+
+
+German Shephard? => Dog
+Lion => Cat 
+
+Dog & Cat => 
+
+
+Animals => Object.methods
+
+
+Reptiles
+Mammals
---
+++ 02-oo-intro/app/models/plant.rb
@@ -0,0 +1,52 @@
+require 'pry'
+# Pry is now within scope
+
+class Plant
+    # binding.pry
+    attr_reader :type, :height
+    attr_accessor :name
+
+    def initialize(name, type, height=10)
+        @name = name 
+        @type = type
+        @height = height
+    end
+
+    # def name 
+    #     @name
+    # end
+    # def height
+    #     @height
+    # end
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+
+end
+
+plant = Plant.new("Okra", "Annual")
+
+binding.pry
+
+# def first_method
+#     puts "this is the first method"
+#     second_method
+# end
+
+# def second_method
+#     puts "this is the second method"
+# end
+
+# second_method
+# first_method
+
+# goes to the 'pry' code, looks for binding.pry and executes it 
+
+# block hasn't been completed yet
+
+# WE NEVER INVOKE THE METHOD
+# .new method does two things:
+    # 1. create a version of yourself (instance)
+    # 2. Look for an initialize method. If one exists for the instance, invoke it
