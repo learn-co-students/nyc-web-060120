c01eec26e6651075612d6806f9c21456d6811b8d
Dwire <gjdwyer8@gmail.com>
2020-07-08 12:27:20 -0400

    minor error handling in sessions controller for wrong username

--- 16-authentication-authorization/cafe_del_ray/app/controllers/sessions_controller.rb
+++ 16-authentication-authorization/cafe_del_ray/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
 
-    if user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user
     else  
