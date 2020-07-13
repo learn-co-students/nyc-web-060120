8ba4fbf38bef7c50fe0b64aea804778b804e0b56
Steven Doran <steven.doran@flatironschool.com>
2020-07-13 12:00:21 -0400

    post lecture

--- 18-intro-to-js/src/index.js
+++ 18-intro-to-js/src/index.js
@@ -1,4 +1,5 @@
 console.log("Welcome to Mod 3")
+// alert("hey there")
 
 /*
  **Questions
@@ -7,21 +8,28 @@ console.log("Welcome to Mod 3")
  **Why JS?
 
   - three pillars - Coffee Dad Twitter
-  - Single Page Applications
+  - 1. DOM Manipulation - what the user sees on the web page
+  - 2. Event handling - doing something when certain things happen in the browser
+  - 3. Fetch - making HTTP requests to an external data source
+
+  - Single Page Applications 
+    - a dynamic web application experience that doesn't require a page reload
+    - all the DOM manipulation and network requests are handled by JS
 
  
-
-
  **What is JS?
 
  - ECMAScript
+  - European Computer Manufacturing Association
  - browser engines
 
 
  **How JS?
 
   - how to load JS into an HTML document
-
+  - inline script tags in our html
+  - local js files we bring in through the src attribute of a script tag
+  - remote js files...
 
 
  **Some data types
