637f7ffa070a7962def199ee51f4a3dffee6c9c1
Steven Doran <steven.doran@flatironschool.com>
2020-07-13 10:44:23 -0400

    pre lecture

---
+++ 18-intro-to-js/src/index.js
@@ -0,0 +1,67 @@
+console.log("Welcome to Mod 3")
+
+/*
+ **Questions
+
+
+ **Why JS?
+
+  - three pillars - Coffee Dad Twitter
+  - Single Page Applications
+
+ 
+
+
+ **What is JS?
+
+ - ECMAScript
+ - browser engines
+
+
+ **How JS?
+
+  - how to load JS into an HTML document
+
+
+
+ **Some data types
+ - null
+
+
+ 
+ - Number
+
+
+ 
+ - String
+
+
+
+ - Object Literals
+
+
+ 
+ - Array
+
+
+
+ - undefined
+
+
+
+ **Functions
+
+
+
+*/
+
+// function declaration, named function
+
+
+
+
+// function expression
+
+
+
+// arrow functions
