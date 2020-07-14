4a92db7501d59ad2150e6cdf8ae877c0016ad36c
Steven Doran <steven.doran@flatironschool.com>
2020-07-14 15:14:55 -0400

    post events lecture

--- 20-intro-to-events/listeners.js
+++ 20-intro-to-events/listeners.js
@@ -1,13 +1,32 @@
 console.log("listening")
 
-// when a user clicks on the welcome image I want to see a turquoise border around that image
+// As a user, when I click on the welcome image I want to see a turquoise border around that image
+// √1. get the image node from the DOM
+// √2. listen for clicks on the node
+// 3. change the style of that node
+
+const welcomeImage = document.getElementById('welcome-image')
+
+welcomeImage.addEventListener('click', function(e){
+  console.dir(e.target)
+  const image = e.target
+  image.style.border = "solid thick turquoise"
+
+})
 
 
 
+// when the mouse moves over the welcome image I want to see a picture of a baby sloth, and i want the picture to change back when the mouse moves off the image
 
-// when the mouse moves over the welcome image i want to see a picture of a baby sloth, and i want the picture to change back when the mouse moves off the image
+welcomeImage.addEventListener("mouseenter", function(e){
+  const image = e.target
+  image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Canadian_lynx_by_Keith_Williams.jpg/238px-Canadian_lynx_by_Keith_Williams.jpg"
+})
 
-
+welcomeImage.addEventListener("mouseleave", function(e){
+  const image = e.target
+  image.src = "https://media.giphy.com/media/UDjF1zMreMld6/giphy.gif"
+})
 
 
 
