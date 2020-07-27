49ca18393b7b3c59e8e709dbaeaa456a82d00618
Steven Doran <steven.doran@flatironschool.com>
2020-07-27 13:37:22 -0400

    pre lecture

---
+++ 33-functions/index.js
@@ -0,0 +1,86 @@
+/*
+
+1. SCOPE
+
+  a. Global Scope
+  b. Function Scope
+  c. Block Scope
+
+2. HOISTING
+
+3. VARIABLE DECLARATION
+
+3. FIRST CLASS FUNCTIONS
+
+  a. callbacks
+  b. closures
+  c. higher order functions
+
+*/
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// 1. SCOPE
+  // a. Global Scope
+
+
+  // b. Function Scope
+
+
+  // c. Block Scope
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+// 2. HOISTING
+
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+// 3. VARIABLE DECLARATION
+  // a. var
+
+
+  // b. let
+
+
+  // c. const
+
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+// 4. FIRST CLASS FUNCTIONS
+
+  /* 
+  languages are said to treat functions as first class citizens 
+  when they treat functions like other objects, meaning they can be
+  assigned to variables, they can be passed around as arguments,
+  or they can be assigned as properties inside objects.
+  */
+
+
+
+  // a. callbacks 
+  // passing a function in as an argument to another function
+  // create my own implementation of the map function
+
+
+
+
+
+  // b. higher order functions
+  // functions that accept functions as parameters or that return other functions
+
+
+
+
+  // c. closures
+  // a closure is when a function is able to remember and access its
+  // scope even when that function is executing outside of its original scope
+  // a function with a backpack, a function with baggage
+
+
+
+
+
